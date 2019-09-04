5cd57045005005a17f2b40c282a412cd306e9210
Diogo Barbosa Silva Sousa <diogo.barbosa@gm5.com.br>
2019-09-04 12:26:07 -0300

    Segundo commit arquivos do projeto controle-series

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/series', function () {
+    $series = [
+        'Grey\'s Anatomy',
+        'Lost',
+        'Agents of Shield'
+     ];
+
+    $html = "<ul>";
+
+    foreach($series as $serie) {
+        $html .= "<li>$serie</li>";
+    }
+
+    $html .= "</ul>";
+
+    return $html;
+});
+
+Route::get('/info', function () {
+    echo phpinfo();
+});
+
