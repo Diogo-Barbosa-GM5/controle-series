103511614e967747d5f13530dbb6aa0cde0099a1
Diogo Barbosa Silva Sousa <diogo.barbosa@gm5.com.br>
2019-09-04 21:20:08 -0300

    Aula 03 - MVC - view - indo para lição 07

--- routes/web.php
+++ routes/web.php
@@ -11,27 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/series', function () {
-    $series = [
-        'Grey\'s Anatomy',
-        'Lost',
-        'Agents of Shield'
-     ];
-
-    $html = "<ul>";
-
-    foreach($series as $serie) {
-        $html .= "<li>$serie</li>";
-    }
-
-    $html .= "</ul>";
-
-    return $html;
-});
+Route::get('/series', 'SeriesController@index');
+Route::get('/series/criar', 'SeriesController@create');
 
 Route::get('/info', function () {
     echo phpinfo();
